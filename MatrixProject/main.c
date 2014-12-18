34d1600f407ca022b9c110a7ab4566fc648c6101
SoMysrie <rethores@gmail.com>
2014-12-18 09:49:15 +0100

    Amélioration du code des matrices
    Ajout de la fonction calcul du tableau générateur

--- MatrixProject/main.c
+++ MatrixProject/main.c
@@ -3,27 +3,31 @@
 
 void displayMatrix(int tab[4][4]);
 void askMatrix(int tab[4][4]);
+void calculGenerator(int tab1[4][4], int tab2[4][4], int tab3[4][8]);
 
 int main(int argc, char **argv)
 {
-    int tab_matrix1[4][4] =
+    int tabMatrixId[4][4] =
     {
         {1, 0, 0, 0},
         {0, 1, 0, 0},
         {0, 0, 1, 0},
         {0, 0, 0, 1}
     };
-    int tab_matrix2[4][4];
+    int tabMatrixAn[4][4];
+    int tabGenerator[4][8];
 
-    askMatrix(tab_matrix2);
+    askMatrix(tabMatrixAn);
 
     //affiche de la matrice par défaut
     printf("\n\nAffichage de la matrice:\n\n");
-    displayMatrix(tab_matrix1);
+    displayMatrix(tabMatrixId);
 
     //affiche de la matrice entrée
     printf("\n\nAffichage de la matrice entree:\n\n");
-    displayMatrix(tab_matrix2);
+    displayMatrix(tabMatrixAn);
+
+    calculGenerator(tabMatrixId, tabMatrixAn, tabGenerator);
 
     return 0;
 }
@@ -64,4 +68,38 @@ void askMatrix(int tab[4][4])
     }
 
     system("cls");
+}
+
+void calculGenerator(int tab1[4][4], int tab2 [4][4], int tab3[4][8])
+{
+    int i, j;
+
+    //ajout de tab1 dans tab3
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            tab3[i][j] = tab1[i][j];
+        }
+    }
+
+    //ajout de tab2 à la suite de tab1 dans tab3
+    for(i=0; i<4; i++)
+    {
+        for(j=4; j<8; j++)
+        {
+            tab3[i][j] = tab2[i][j-4];
+        }
+    }
+
+    //affiche la matrice génératrice
+    printf("\n\nAffichage de la matrice generatrice:\n\n");
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<8; j++)
+        {
+            printf("%d ", tab3[i][j]);
+        }
+        printf("\n");
+    }
 }
