66d86ef7789b5c2bbfdc588088067f9ecb73331d
SoMysrie <rethores@gmail.com>
2014-12-28 12:01:05 +0100

    Développement du chiffrement
    Ajout de la fonction qui permet de choisir si l'utilisateur veut
    chiffrer ou déchiffrer un fichier.

--- MatrixProject/code.c
+++ MatrixProject/code.c
@@ -1,1 +1,19 @@
+#include "header.h"
+
+int choiceMatrix(int *choice)
+{
+    do
+    {
+        printf("\n\nPlease choose\n\n");
+        printf("1. Encode.\n");
+        printf("2. Decode.\n");
+        scanf("%d", choice);
+        printf("\n");
+    }while(choice !=1 && choice != 2);
+}
+
+void calculGenerator(int tab[4][8])
+{
+
+}
 
